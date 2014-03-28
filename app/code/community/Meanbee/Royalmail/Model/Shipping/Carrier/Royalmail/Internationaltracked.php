c456fc65de0c74650739038d05f4594e7e7a9586
Jools Wills <jools@oxfordinspire.co.uk>
2014-03-28 00:29:24 +0000

    cosmetic: consistent commenting

--- app/code/community/Meanbee/Royalmail/Model/Shipping/Carrier/Royalmail/Internationaltracked.php
+++ app/code/community/Meanbee/Royalmail/Model/Shipping/Carrier/Royalmail/Internationaltracked.php
@@ -41,7 +41,7 @@ class Meanbee_Royalmail_Model_Shipping_Carrier_Royalmail_Internationaltracked
             return null;
         }
 
-        # Don't allow international tracked to Australia, Brazil, Canada or Israel
+        // Don't allow international tracked to Australia, Brazil, Canada or Israel
         if($country == 'AU' || $country == 'BR' || $country == 'CA' || $country == 'IL') {
             return null;
         }
